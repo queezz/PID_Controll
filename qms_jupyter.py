05ce8d1b559732af602e40969ccd30c421251e5d
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-01-09 12:13:36 +0900

    fix plot

--- qms_jupyter.py
+++ qms_jupyter.py
@@ -24,13 +24,14 @@ sorted(a)
 
 # %%
 """ csv """
-base_time = datetime(2019, 12, 27, 20, 21, 38)  # pyqt start time
-start_time = datetime(2019, 12, 27, 20, 17, 24) # qms start time
+base_time = datetime(2019, 12, 27, 20, 21, 38)
+start_time = datetime(2019, 12, 27, 20, 17, 24)
 d_time = (start_time - base_time).total_seconds()
 q_csv = pd.read_csv(d + "/S1_191227_201724.csv", header=36, encoding='utf-8')
+column_csv = pd.read_csv(d + "/S1_191227_201724.csv", header=None, encoding='utf-8', skiprows=lambda x: x not in [7])
 print(q_csv.columns)
+print(column_csv)
 print(q_csv)
-
 
 # %%
 """ calculate time """
@@ -44,28 +45,36 @@ calc_time('000:55:45.719')
 # %%
 data = np.array(q_csv)
 data[:, 1] = [calc_time(i) for i in data[:, 1]]
+print(data[-1, 1])
 
-""" plot qms """
+data_column  = np.array(column_csv)
+data_column = np.array(column_csv)[0, 1:15]
+print(data_column)
+
 def plot_qms(data: np.ndarray, gray_start: datetime=None, gray_end: datetime=None, red_start: datetime=None, red_end: datetime=None):
     import matplotlib.pyplot as plt
     # total pressure
     plt.title("total pressure")
     plt.xlabel("Time [s]")
-    plt.ylabel("total pressure [Pa]")
+    plt.ylabel("total pressure [A]")
     plt.yscale('log')
     plt.axvspan(gray_start, gray_end, color="gray", alpha=0.3)
     plt.axvspan(red_start, red_end, color="red", alpha=0.3)
     plt.plot(data[:, 1], data[:, 4])
     plt.figure()
 
+    plt.title("pressure")
+    plt.xlabel("Time [s]")
+    plt.ylabel("pressure [A]")
+    plt.yscale('log')
+    cmap = plt.get_cmap("tab20")
+
     for i in range(1, 15):
-        plt.title("{} pressure".format(i))
-        plt.xlabel("Time [s]")
-        plt.ylabel("{} pressure [Pa]".format(i))
-        plt.yscale('log')
-        plt.axvspan(gray_start, gray_end, color="gray", alpha=0.3)
-        plt.axvspan(red_start, red_end, color="red", alpha=0.3)
-        plt.plot(data[:, 1], data[:, i+4])
-        plt.figure()
+        plt.axvspan(gray_start, gray_end, color=cmap(15), alpha=0.3)
+        plt.axvspan(red_start, red_end, color=cmap(17), alpha=0.3)
+        plt.plot(data[:, 1], data[:, i+4], color=cmap(i), label="{}".format(data_column[i-1]))
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, fontsize=18)
 
 plot_qms(data)
+
+# %%
